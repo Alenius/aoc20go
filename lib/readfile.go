ed43e8dacad16abf158213a2820dd5578072cb2c
adam alenius <adam.alenius@gmail.com>
2022-10-02 11:07:18 +0200

    add regexp helper func

--- lib/readfile.go
+++ lib/readfile.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 // fn takes in the line and does something with it
@@ -27,4 +28,29 @@ func ReadFileByLine(day string, fn func(string)) error {
 	}
 
 	return nil
+}
+
+// returns map of named subgroups in a regex on format subgroupname to regexpmatch
+func FindNamedSubgroupInString(s string, re *regexp.Regexp, names []string) (map[string]string, error) {
+	matches := map[string]string{}
+	for _, name := range names {
+		ix := re.SubexpIndex(name)
+		if ix < 0 {
+			return nil, fmt.Errorf("could not find any subgroup in regexp with name: %v", name)
+		}
+		match := re.FindStringSubmatch(s)
+		if match == nil {
+			return nil, fmt.Errorf("no match for in regexp: %v", name)
+		}
+
+		subexpix := re.SubexpIndex(name)
+		if subexpix < 0 {
+			return nil, fmt.Errorf("no match on subgroup with name: %v", name)
+		}
+
+		submatch := match[subexpix]
+		matches[name] = submatch
+	}
+
+	return matches, nil
 }
